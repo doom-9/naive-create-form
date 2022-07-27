468b17290628d9bfdc587ae1e8c4adbd69f24709
doom-0726 <110001160+doom-9@users.noreply.github.com>
2022-07-27 15:46:34 +0800

    feat: add test

--- lib/components/proForm/test/index.test.ts
+++ lib/components/proForm/test/index.test.ts
@@ -1,9 +1,28 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
 import ProForm from '../../proForm/index'
 
 describe('suite name', () => {
   test('should work with import on demand', () => {
     mount(ProForm)
+  })
+
+  test('should work with title prop', async () => {
+    const w = mount(ProForm, {
+      props: {
+        title: 'test Title',
+      },
+    })
+    expect(w.find('.n-divider').exists()).toBe(true)
+
+    expect(w.find('.n-divider__title').text()).toEqual('test Title')
+
+    await w.setProps({
+      title: undefined,
+    })
+
+    expect(w.find('.n-divider').exists()).toBe(false)
+
+    w.unmount()
   })
 })
